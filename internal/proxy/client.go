fc8ff9b17bd070548afaa88122541bad2790ce7f
Valentyna Bukhalova <valentyna@cast.ai>
2024-10-01 11:37:56 +0200

    fix

--- internal/proxy/client.go
+++ internal/proxy/client.go
@@ -285,6 +285,7 @@ func (c *Client) handleMessage(ctx context.Context, in *cloudproxyv1alpha.Stream
 		Request: &cloudproxyv1alpha.StreamCloudProxyRequest_Response{
 			Response: &cloudproxyv1alpha.ClusterResponse{
 				ClientMetadata: &cloudproxyv1alpha.ClientMetadata{
+					PodName:   c.streamRuntimeName,
 					ClusterId: c.clusterID,
 				},
 				MessageId:    in.GetMessageId(),
